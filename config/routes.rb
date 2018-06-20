98c14795a4553d3c35a6834865108779d6822cf7
fumimi <info@fumimi.jp>
2018-06-20 22:02:31 +0900

    Scheduleコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'schedules/new'
+
+  get 'schedules/create'
+
+  get 'schedules/index'
+
+  get 'schedules/show'
+
   get 'static_pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
